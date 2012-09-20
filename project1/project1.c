ca3db9467b0d280db575c94d7381c9e0cb6a4824
Liam Morris <lcm1115@rit.edu>
2012-09-20 14:52:10 -0400

    Commented files

--- project1/project1.c
+++ project1/project1.c
@@ -1,3 +1,16 @@
+/* File: project1.c
+** Author: Liam Morris - lcm1115
+** Description: Implements a basic course and student database. Allows for
+**              adding new courses, adding new students, registering students
+**              for courses, dropping students from courses, and removing
+**              courses from the system.
+**
+**              Usage:
+**                project1 [-v] courses.file students.file
+**
+**              If the flag -v is set, verbose mode is enabled which prints out
+**              messages when certain commands finish.
+*/
 #include "allocate.h"
 #include "project1.h"
 #include <stdio.h>
@@ -5,10 +18,16 @@
 #include <string.h>
 #include <unistd.h>
 
+// Verbosity flag
 uint8_t verbose;
+
+// Heads of student and course linked lists.
 struct Student* shead = 0;
 struct Course* chead = 0;
+
+// Input buffer
 char* input;
+
 int main(int argc, char** argv) {
   input = allocate(MAX_BUFFER_LENGTH);
   if (!input) {
@@ -17,15 +36,18 @@ int main(int argc, char** argv) {
     exit(-1);
   }
   char* temp;
+  // Checks that flags are valid.
   if (argc < 3 || argc > 4 || (argc == 4 && strcmp(argv[1], "-v"))) {
     fprintf(stderr, "usage: project1 [-v] course_file student_file\n");
     return 1;
   }
+  // Set verbosity if necessary.
   if (argc == 4) {
     verbose = 1;
   }
   temp = argv[argc - 2];
 
+  // Read in course and student files
   FILE* fp;
 READ:
   fp = fopen(temp, "r");
@@ -35,6 +57,7 @@ READ:
     exit(-1);
   }
 
+  // Read each line and process call the corresponding function.
   while (fgets(input, MAX_BUFFER_LENGTH, fp) != NULL) {
     input[strlen(input) - 1] = 0;
     char* test = strtok(input, " \t");
@@ -46,13 +69,18 @@ READ:
   }
 
   fclose(fp);
+  // If student file hasn't been read yet, read it.
   if (strcmp(temp, argv[argc - 1])) {
     temp = argv[argc - 1];
     goto READ;
   }
 
+  // Read commands until no input is entered.
   while (fgets(input, MAX_BUFFER_LENGTH, stdin)) {
+    // Strip \n from end of the command.
     input[strlen(input) - 1] = 0;
+
+    // Call appropriate function depending on which command was issued.
     temp = strtok(input, " \t");
     if (!strcmp(temp, "student")) {
       addstudent();
@@ -61,6 +89,7 @@ READ:
     } else if (!strcmp(temp, "add")) {
       enrollstudent();
     } else if (!strcmp(temp, "drop")) {
+      // Parse student and course ID information and then call dropstudent.
       int sid = atof(strtok(NULL, " \t"));
       char* temp = strtok(NULL, " \t");
       char tempid[3];
@@ -77,13 +106,20 @@ READ:
       removecourse();
     }
   }
+
+  // After all input has been processed, print out information about the
+  // students and courses.
   struct Student* curs = shead;
   struct Course* curc = chead;
   struct Enrollment* cur;
+
+  // Iterate across students list
   while (curs) {
     printf("\n");
     printf("Student ");
     printstudent(&curs);
+
+    // If a student is taking courses, print them.
     if (numcourses(&curs) <= 0) {
       printf(" is taking no courses");
     } else {
@@ -97,12 +133,16 @@ READ:
     curs = curs->next;
     printf("\n");
   }
+
+  // Iterate across course lists.
   while (curc) {
     printf("\n");
     printf("Course %s%03d (limit %d) ",
            curc->depid,
            curc->cid,
            curc->size);
+
+    // If students are enrolled in the course, print them.
     if (numenrolled(&curc) <= 0) {
       printf("is empty\n");
     } else {
@@ -117,6 +157,8 @@ READ:
     }
     curc = curc->next;
   }
+
+  // Unallocate all memory that was allocated.
   clearmem();
 }
 
@@ -125,6 +167,8 @@ void clearmem() {
   struct Student* prevs;
   struct Enrollment* cure;
   struct Enrollment* preve;
+  // Iterate across the student list and delete each student.
+  // Additionally, delete all enrollments.
   while (curs) {
     prevs = curs;
     curs = curs->next;
@@ -134,12 +178,16 @@ void clearmem() {
       cure = cure->nextcourse;
       unallocate(preve);
     }
+
+    // Unallocate all dynamically allocated memory from the student, then
+    // delete the actual student.
     unallocate(prevs->first);
     unallocate(prevs->middle);
     unallocate(prevs->last);
     unallocate(prevs);
   }
 
+  // Iterate across the course and delete each course.
   struct Course* curc = chead;
   struct Course* prevc;
   while (curc) {
@@ -151,12 +199,15 @@ void clearmem() {
 }
 
 void strcopy(char** dest, char** src) {
+  // Allocate strlen + 1 bytes for the new buffer (to account for NULL-byte)
   *dest = allocate(strlen(*src) + 1);
   if (!dest) {
     printf("Memory allocation failed!\n");
     clearmem();
     exit(-1);
   }
+
+  // Copy contents
   strcpy(*dest, *src);
 }
 
@@ -166,6 +217,8 @@ void addstudent() {
   int sid;
   char* temp;
 
+  // Extract sid from input buffer and make sure that student doesn't already
+  // exist.
   temp = strtok(NULL, " \t");
   sid = atof(temp);
   if (getstudent(sid)) {
@@ -173,6 +226,7 @@ void addstudent() {
     return;
   }
 
+  // Allocate memory for the new student and initialize all fields.
   new = allocate(sizeof(struct Student));
   if (!new) {
     printf("Memory allocation failed!\n");
@@ -186,6 +240,7 @@ void addstudent() {
   new->next = 0;
   new->sid = sid;
 
+  // Extract all names possible from input.
   temp = strtok(NULL, " \t");
   strcopy(&(new->last), &temp);
   temp = strtok(NULL, " \t");
@@ -197,13 +252,20 @@ void addstudent() {
     strcopy(&(new->middle), &temp);
   }
 
+  // If student linked list is empty, make student the new head.
   if (shead == NULL) {
     shead = new;
-  } else if (comparestudent(&new, &shead) < 0) {
+  }
+  // If the student is "less than" the head, make it the new head.
+  else if (comparestudent(&new, &shead) < 0) {
     new->next = shead;
     shead = new;
-  } else {
+  }
+  // Otherwise, place the student where it belongs in the list.
+  else {
     cur = shead;
+    // Iterate until the end of the list is found or the current position is
+    // the proper position for the new student.
     while (cur->next && comparestudent(&cur->next, &new) < 0) {
       cur = cur->next;
     }
@@ -226,6 +288,8 @@ void addcourse() {
   char* temp;
   char tempid[3];
   short cid;
+
+  // Parse course ID information and make sure it doesn't already exist.
   temp = strtok(NULL, " \t");
   tempid[0] = temp[0];
   tempid[1] = temp[1];
@@ -237,6 +301,7 @@ void addcourse() {
     return;
   }
 
+  // Allocate new course and initialize all fields.
   new = allocate(sizeof(struct Course));
   if (!new) {
     printf("Memory allocation failed!\n");
@@ -251,19 +316,27 @@ void addcourse() {
   new->cid = cid;
   new->size = atof(strtok(NULL, " \t"));
 
+  // If course list is currently empty, make the new course the head.
   if (chead == NULL) {
     chead = new;
-  } else if (strcmp(new->depid, chead->depid) < 0 ||
+  }
+  // If the course is "less than" the current head, make it the new head.
+  else if (strcmp(new->depid, chead->depid) < 0 ||
              (!(strcmp(new->depid, chead->depid)) &&
                new->cid < chead->cid)) {
     new->next = chead;
     chead = new;
-  } else {
+  }
+  // Otherwise, insert the course in its proper position.
+  else {
     cur = chead;
+    // Find approximate position by department ID.
     while (cur->next && strcmp(cur->next->depid, new->depid) < 0) {
       cur = cur->next;
     }
+    // If at the end of the list, put new course on the end of the list.
     if (!cur->next) cur->next = new;
+    // Otherwise, find exact position by course ID within department ID.
     else {
       while (cur->next && !strcmp(new->depid, cur->next->depid) &&
              cur->next->cid < new->cid) {
@@ -283,10 +356,13 @@ void addcourse() {
 }
 
 void enrollstudent() {
+  // Get pointer to student.
   int sid = atof(strtok(NULL, " \t"));
   struct Student* student = getstudent(sid);
   struct Enrollment* cur;
   struct Course* course;
+
+  // Get pointer to course.
   char* temp = strtok(NULL, " \t");
   char tempid[3];
   short cid;
@@ -299,6 +375,12 @@ void enrollstudent() {
 
   course = getcourse(tempid, cid);
 
+  // Check that:
+  //     Student exists
+  //     Course exists
+  //     Student has room in schedule
+  //     Course has room in roster
+  //     Student isn't already enrolled in course.
   if (!student) {
     fprintf(stderr, "%05d does not exist\n", sid);
   } else if (!course) {
@@ -313,6 +395,7 @@ void enrollstudent() {
             course->depid,
             course->cid);
   } else {
+    // Allocate enrollment and initialize all fields.
     struct Enrollment* enrollment = allocate(sizeof(struct Enrollment));
     if (!enrollment) {
       printf("Memory allocation failed!\n");
@@ -323,14 +406,20 @@ void enrollstudent() {
     enrollment->nextcourse = 0;
     enrollment->student = student;
     enrollment->course = course;
+    // If student's schedule is empty, make this course head of the list.
     if (student->clist == NULL) {
       student->clist = enrollment;
-    } else if (strcmp(enrollment->course->depid, student->clist->course->depid) < 0 ||
+    }
+    // If first entry in student's schedule is greater than new enrollment,
+    // make the new enrollment the first entry.
+    else if (strcmp(enrollment->course->depid, student->clist->course->depid) < 0 ||
                (!(strcmp(enrollment->course->depid, student->clist->course->depid)) &&
                  enrollment->course->cid < student->clist->course->cid)) {
       enrollment->nextcourse = student->clist;
       student->clist = enrollment;
-    } else {
+    }
+    // Otherwise insert course in proper location in schedule.
+    else {
       cur = student->clist;
       while (cur->nextcourse &&
         strcmp(cur->nextcourse->course->depid, enrollment->course->depid) < 0) {
@@ -349,12 +438,19 @@ void enrollstudent() {
         cur->nextcourse = enrollment;
       }
     }
+    // If course roster is empty, make new enrollment the first entry.
     if (course->slist == NULL) {
       course->slist = enrollment;
-    } else if (comparestudent(&enrollment->student, &course->slist->student) < 0) {
+    }
+    // If first student in roster comes after new enrollment's student
+    // alphabetically, make the new enrollment the new first entry.
+    else if (comparestudent(&enrollment->student, &course->slist->student) < 0) {
       enrollment->nextstudent = course->slist;
       course->slist = enrollment;
-    } else {
+    }
+    // Otherwise insert the new enrollment in to the roster in the proper
+    // position (alphabetically by student).
+    else {
       cur = course->slist;
       while (cur->nextstudent &&
         comparestudent(&cur->nextstudent->student, &enrollment->student) < 0) {
@@ -365,6 +461,7 @@ void enrollstudent() {
       }
       cur->nextstudent = enrollment;
     }
+
     if (verbose) {
       printf("%05d added to %s%03d\n",
              enrollment->student->sid,
@@ -377,10 +474,15 @@ void enrollstudent() {
 void dropstudent(int sid,
                  char depid[2],
                  short cid) {
+  // Get pointers to student and course.
   struct Student* student = getstudent(sid);
   struct Course* course = getcourse(depid, cid);
   struct Enrollment* todelete;
 
+  // Check that:
+  //     Student exists
+  //     Course exists
+  //     Student is enrolled in the course
   if (!student) {
     fprintf(stderr, "%05d does not exist\n", sid);
   } else if (!course) {
@@ -391,6 +493,8 @@ void dropstudent(int sid,
             course->depid,
             course->cid);
   } else {
+    // Find enrollment listing in course's roster and remove it from the linked
+    // list.
     struct Enrollment* cur = course->slist;
     if (!cur->nextstudent || cur->student->sid == student->sid) {
       course->slist = cur->nextstudent;
@@ -400,6 +504,8 @@ void dropstudent(int sid,
       }
       cur->nextstudent = cur->nextstudent->nextstudent;
     }
+    // Find enrollment listing in student's schedule and remove it from the
+    // linked list.
     cur = student->clist;
     if (!cur->nextcourse || cur->course == course) {
       todelete = student->clist;
@@ -418,6 +524,7 @@ void dropstudent(int sid,
              course->depid,
              course->cid);
     }
+    // Delete enrollment.
     unallocate(todelete);
   }
 }
@@ -425,6 +532,8 @@ void dropstudent(int sid,
 void removecourse() {
   struct Course* course;
   struct Enrollment* enrollment;
+
+  // Get department and course ID and get a pointer to the course.
   char* temp = strtok(NULL, " \t");
   char tempid[3];
   short cid;
@@ -437,20 +546,25 @@ void removecourse() {
 
   course = getcourse(tempid, cid);
 
+  // Make sure course exists.
   if (!course) {
     printf("%s%03d does not exist\n", tempid, cid);
     return;
   }
 
   enrollment = course->slist;
+
+  // Disable verbosity while dropping students.
   uint8_t v = verbose;
   verbose = 0;
   while (enrollment != NULL && enrollment->student != NULL) {
+    // Drop all students in the course's roster.
     dropstudent(enrollment->student->sid,
                 course->depid,
                 course->cid);
     enrollment = enrollment->nextstudent;
   }
+  // Remove course from course list.
   if (!strcmp(course->depid, chead->depid) &&
       course->cid == chead->cid) {
     chead = chead->next;
@@ -466,11 +580,13 @@ void removecourse() {
   if (verbose) {
     printf("%s%03d closed\n", course->depid, course->cid);
   }
+  // Delete course.
   unallocate(course);
 }
 
 struct Student* getstudent(int sid) {
   struct Student* cur = shead;
+  // Iterate through linked list until student ID is found.
   while (cur && cur->sid != sid) {
     cur = cur->next;
   }
@@ -479,13 +595,16 @@ struct Student* getstudent(int sid) {
 
 struct Course* getcourse(char depid[2], short cid) {
   struct Course* cur = chead;
+  // Iterate through linked list until department ID and course ID is found.
   while (cur && (strcmp(depid, cur->depid) || cid != cur->cid)) {
     cur = cur->next;
   }
   return cur;
 }
 
-int hasstudent(struct Course** course, int sid) {
+uint8_t hasstudent(struct Course** course, int sid) {
+  // Iterate through roster until student with specified student ID is found.
+  // Return 1 if it is found, return 0 otherwise.
   struct Enrollment* cur = (*course)->slist;
   while (cur && cur->student->sid != sid) {
     cur = cur->nextstudent;
@@ -498,6 +617,8 @@ int hasstudent(struct Course** course, int sid) {
 }
 
 void printstudent(struct Student** student) {
+  // Prints a student ID and name(s) in the following format:
+  // SID (Last [First [Middle]])
   printf("%05d (%s", (*student)->sid, (*student)->last);
   if ((*student)->first != 0) printf(" %s", (*student)->first);
   if ((*student)->middle != 0) printf(" %s", (*student)->middle);
@@ -505,21 +626,25 @@ void printstudent(struct Student** student) {
 }
 
 int comparestudent(struct Student** stu1, struct Student** stu2) {
+  // Compare last names.
   if (strcmp((*stu1)->last, (*stu2)->last) < 0) {
     return -1;
   } else if (strcmp((*stu1)->last, (*stu2)->last) > 0) {
     return 1;
   } else {
+    // Compare first names.
     if (strcmp((*stu1)->first, (*stu2)->first) < 0) {
       return -1;
     } else if (strcmp((*stu1)->first, (*stu2)->first) > 0) {
       return 1;
     } else {
+      // Compare middle names.
       if (strcmp((*stu1)->middle, (*stu2)->middle) < 0) {
         return -1;
       } else if (strcmp((*stu1)->middle, (*stu2)->middle) > 0) {
         return 1;
       } else {
+        // Compare student ID.
         return (*stu1)->sid - (*stu2)->sid;
       }
     }
@@ -527,6 +652,7 @@ int comparestudent(struct Student** stu1, struct Student** stu2) {
 }
 
 uint8_t numenrolled(struct Course** course) {
+  // Iterate through linked list and count how many students are looked at.
   struct Enrollment* cur = (*course)->slist;
   uint8_t numenrolled = 0;
   while (cur) {
@@ -537,6 +663,7 @@ uint8_t numenrolled(struct Course** course) {
 }
 
 uint8_t numcourses(struct Student** student) {
+  // Iterate through linked list and count how many courses are looked at.
   struct Enrollment* cur = (*student)->clist;
   uint8_t numcourses = 0;
   while (cur) {
--- project1/project1.h
+++ project1/project1.h
@@ -17,6 +17,8 @@ struct Student {
   char* first;
   char* middle;
   char* last;
+
+  // Representation of course schedule for a student.
   struct Enrollment* clist;
   struct Student* next;
 };
@@ -28,10 +30,16 @@ struct Course {
   char depid[3];
   short cid;
   uint8_t size;
+
+  // Representation of a student roster for a course.
   struct Enrollment* slist;
   struct Course* next;
 };
 
+// Representation of an "Enrollment" in a linked list.
+// When a student registers for a course, an Enrollment is initialized with the
+// course and the student, as well as any pointers to the next courses or
+// students in the enrollment linked list.
 struct Enrollment {
   struct Student* student;
   struct Course* course;
@@ -40,32 +48,55 @@ struct Enrollment {
 };
 
 // Copies a string from one buffer to another.
+// Allocates memory for destination buffer before copying.
 void strcopy(char** dest, char** src);
 
 // Adds a student to the database.
 void addstudent();
 
+// Adds a course to the database.
 void addcourse();
 
+// Enrolls a student in a course.
 void enrollstudent();
 
+// Drops a student from a course.
 void dropstudent(int sid, char depid[2], short cid);
 
+// Removes a course from a database and removes it from all students' schedules
+// that were enrolled in it.
 void removecourse();
 
+// Returns a pointer to a student with a specificed ID.
+// Returns NULL if student does not exist.
 struct Student* getstudent(int sid);
 
+// Returns a pointer to a course with a specified department and course ID.
+// Returns NULL if course does not exist.
 struct Course* getcourse(char depid[2], short cid);
 
-int hasstudent(struct Course** course, int sid);
+// Determines whether a student (with a specified ID) is enrolled in a course.
+// Returns 1 if the student is found, 0 otherwise.
+uint8_t hasstudent(struct Course** course, int sid);
 
+// Prints out a student to the console in one of the following formats:
+//     sid (name)
+//     sid (last first)
+//     sid (last first middle)
 void printstudent(struct Student** student);
 
+// Compares two students for equality.
+// Student fields are compared in the order last > first > middle > sid.
+// If stu1 is less than stu2, -1 is returned. If stu2 is less than stu1, 1 is
+// returned.
 int comparestudent(struct Student** stu1, struct Student** stu2);
 
+// Deallocates all Student, Course, and Enrollment instances.
 void clearmem();
 
+// Returns the number of students enrolled in a course.
 uint8_t numenrolled(struct Course** course);
 
+// Returns the number of courses a student is enrolled in.
 uint8_t numcourses(struct Student** student);
 #endif
