096dc92da1057f04a9ea08662ce0a762037e8949
Liam <liam@taggart.(none)>
2012-10-18 15:36:50 -0400

    Added base functionality for program flow

--- project3/main.c
+++ project3/main.c
@@ -10,14 +10,21 @@
 ** Description:	Dummy main program
 */
 #include "c_io.h"
+#include "rngs.h"
 
 int main( void ) {
-        char seed[13];
-	c_puts( "Hello, world!\n" );
-
+        char seed[12];
         c_puts("Team:\nLiam Morris\n");
         c_puts("Enter twelve-character seed: ");
-        c_gets(seed, 12);
+        for (int i = 0; i < 12; ++i) {
+          unsigned int ch = c_getchar();
+          if (ch == 10 || ch == 15) {
+            --i;
+            c_putchar(7);
+          } else {
+            seed[i] = ch;
+          }
+        }
         
         unsigned int vals[3];
 
@@ -32,5 +39,31 @@ int main( void ) {
 
         PutSeed(sd);
 
+        while (1) {
+          c_puts("\nPress the enter key when you are ready\n");
+          unsigned int input = 0;
+          while (input != 10 && input != 15) {
+            input = c_getchar();
+            c_putchar(10);
+          }
+          int ch = Random() * 25 + 'a';
+          c_putchar(7);
+          c_putchar(ch);
+          do {
+            input = c_getchar();
+            c_putchar(10);
+          } while (input != ch);
+          c_puts("Another try?\n");
+          do {
+            input = c_getchar();
+            c_putchar(10);
+            if (input != 'y' && input != 'n') {
+              c_putchar(7);
+            }
+          } while (input != 'y' && input != 'n');
+          if (input == 'n') {
+            break;
+          }
+        }
 	return( 0 );
 }
