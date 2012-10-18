ba614dc18fd56912c01f03f7e902fb88e6e75a33
Liam <liam@taggart.(none)>
2012-10-18 14:30:32 -0400

    Initial revision

--- project3/main.c
+++ project3/main.c
@@ -12,6 +12,25 @@
 #include "c_io.h"
 
 int main( void ) {
+        char seed[13];
 	c_puts( "Hello, world!\n" );
+
+        c_puts("Team:\nLiam Morris\n");
+        c_puts("Enter twelve-character seed: ");
+        c_gets(seed, 12);
+        
+        unsigned int vals[3];
+
+        for (int i = 0; i < 3; ++i) {
+          vals[i] = 0;
+          for (int j = 0; j < 4; j++) {
+            vals[i] |= seed[i * 4 + j] << (8 * j);
+          }
+        }
+
+        unsigned int sd = vals[0] ^ vals[1] ^ vals[2];
+
+        PutSeed(sd);
+
 	return( 0 );
 }
